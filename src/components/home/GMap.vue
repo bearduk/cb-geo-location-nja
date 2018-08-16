2f2dbdaeb63f4082511b0dca61403dcff193d5de
Chris Beard <coebe500@aol.com>
2018-08-16 18:13:32 +0100

    GMap component

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="map">
+        <h2>Map component</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GMap',
+        data(){
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
