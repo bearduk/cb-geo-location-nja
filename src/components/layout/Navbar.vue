54f652776d22d4b2da61a8f97100a011e5718ae3
Chris Beard <coebe500@aol.com>
2018-08-18 16:11:31 +0100

    Conditionals on login and out by detecting when user auth changes onAuthStateChanged

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -4,9 +4,10 @@
             <div class="container">
                 <router-link :to="{name: 'GMap'}" class="brand-logo left">Geo location app</router-link>
                 <ul class="right">
-                    <li><router-link :to="{name: 'Signup'}">Signup</router-link></li>
-                    <li><router-link :to="{name: 'Login'}">Login</router-link></li>
-                    <li><a @click="logout" class="">Logout</a></li>
+                    <li v-if="!user"><router-link :to="{name: 'Signup'}">Signup</router-link></li>
+                    <li v-if="!user"><router-link :to="{name: 'Login'}">Login</router-link></li>
+                    <li v-if="user"><a href="">{{user.email}}</a></li>
+                    <li v-if="user"><a @click="logout" class="">Logout</a></li>
                 </ul>
             </div>
         </nav>
@@ -21,7 +22,7 @@ import firebase from 'firebase'
         name: 'Navbar',
         data(){
             return{
-
+                user: null
             }
         },
         methods:{
@@ -31,6 +32,16 @@ import firebase from 'firebase'
                     this.$router.push({name: 'Login'})
                 })
             }
+        },
+        created(){
+            // let user = firebase.auth().currentUser
+            firebase.auth().onAuthStateChanged( (user) => {
+                if (user){
+                    this.user = user
+                } else {
+                    this.user = null
+                }
+            } )
         }
     }
 </script>
